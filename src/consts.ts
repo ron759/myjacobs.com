7a06adafc611240d29ad5dbd786bc88e178f9524
Ron Jacobs <ron@myjacobs.com>
2026-04-25 22:45:48 -0500

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'The Jacobs Family';
+export const SITE_DESCRIPTION = 'Love God, Love Family, Love People.';
