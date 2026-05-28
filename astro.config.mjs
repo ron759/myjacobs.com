3cfdbc169440ad01dafb3523a25cccec3ea067fa
Ron Jacobs <ron@myjacobs.com>
2026-05-28 15:39:21 -0500

    moved to cloudinary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	output: 'server',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
